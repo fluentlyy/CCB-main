68d3774937f9fb244d197a9cdecd48ce2cd0b22b
fluentlyy <108574339+fluentlyy@users.noreply.github.com>
2024-12-14 13:08:46 +0200

    123

--- About Us/main.js
+++ About Us/main.js
@@ -5,6 +5,14 @@ let contentParagraph = document.querySelector(".content__paragraph");
 let contentInfo = document.querySelector(".content__info");
 function handleMediaChange() {
   if (window.innerWidth <= 730) {
+    contentInfo.innerHTML = `<div class="about__us-content">
+  <h1 class="about__us-title">about us</h1>
+  <p class="about__us-paragraph">
+    Cultura Contra Balas (CCB) is a non-profit organization that uses culture as
+    a tool against violence.
+  </p>
+</div>
+<img src="./image/Golub.krula 1.png" alt="" class="about__us-img" />`;
     img.src = "../Team/img/founder.png";
     founderTitle.textContent = "our Founder";
     contentTitle.textContent = "Natalia Batkivna";
@@ -24,20 +32,6 @@ function handleMediaChange() {
   encourage everyone to spread kindness and inspire others!
 </p>`;
   } else if (window.innerWidth <= 1024) {
-    `<div class="top__title">
-  <h1>headline</h1>
-  <p class="top__text">
-    Through cultural and educational events, CCB unites Spain and countries
-    affected by armed conflict. The NGO is a space for psychological
-    rehabilitation and development. All of its projects are based on the work
-    and support of dedicated volunteers. Led by a strong belief in humanity, we
-    encourage everyone to spread kindness and inspire others!
-  </p>
-</div>
-<div class="title__imgs">
-  <img src="./image/holub.flamenko (1) 1.png" alt="" class="img__flamenko" />
-  <img src="./image/Mask group.png" alt="" class="img__guitar" />
-</div>`;
     contentInfo.innerHTML = `<div class="about__us-content">
   <h1 class="about__us-title">about us</h1>
   <p class="about__us-paragraph">
